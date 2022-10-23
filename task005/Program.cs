848f2507f30de0295188007b2f16d16f2e7404e1
Jalini <jalini@mail.ru>
2022-10-23 23:53:26 +0300

    jjsssss

---
+++ task005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int task =0-num;
+//Console.Write(task);
+while (task<=num)
+{
+    Console.Write(task);
+    task++;
+}
