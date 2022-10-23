945c5ec68e9c0433d13e04fbcd52bbaaf1228fa7
Jalini <jalini@mail.ru>
2022-10-24 01:50:32 +0300

    jjsssssdsdsjjjkk

---
+++ 002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter figures: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (x>y)
+{
+max=x;
+}
+else
+{
+    max=y;
+}
+if (max>z)
+{
+    Console.WriteLine("max figure: " +max);
+}
+else Console.WriteLine("max figure: " +z);
