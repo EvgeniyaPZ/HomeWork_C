4fd2c9e01724184fba41528bf3eaf184a83b8d91
Jalini <jalini@mail.ru>
2022-10-24 01:04:09 +0300

    jjsssssdsds

---
+++ lastnumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+string num = Console.ReadLine();
+int count = num.Length;
+if (count !=3)
+{
+    Console.WriteLine("Error! Number not 3 sImbol!");
+}
+else
+{
+    Console.WriteLine(num.Substring(2,1));
+}
