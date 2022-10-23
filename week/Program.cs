d4da1b66328807da3112dc0b86e2643db8809d82
Jalini <jalini@mail.ru>
2022-10-23 23:16:08 +0300

    jjss

---
+++ week/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter number week: ");
+int week = Convert.ToInt32(Console.ReadLine());
+
+if (week ==1)
+{
+    Console.WriteLine("Monday");
+}
+else if (week==2)
+{
+   Console.WriteLine("Tuesday"); 
+}
+else if (week==3)
+{
+   Console.WriteLine("Wensday"); 
+}
+else if (week==4)
+{
+   Console.WriteLine("Thesday"); 
+}
+else if (week==5)
+{
+   Console.WriteLine("Friday"); 
+}
+else if (week==6)
+{
+   Console.WriteLine("Saturday"); 
+}
+else if (week==7)
+{
+   Console.WriteLine("Sunday"); 
+}
+else
+{
+   Console.WriteLine("Error"); 
+}
