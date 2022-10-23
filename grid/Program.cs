a04d44be588cb2e8468961c068f794754de08f6e
Jalini <jalini@mail.ru>
2022-10-23 22:54:03 +0300

    jjkkk

--- grid/Program.cs
+++ grid/Program.cs
@@ -1,4 +1,22 @@
-﻿Console.WriteLine("Enter number: ");
-int num= Convert.ToInt32(Console.ReadLine());
-int grid = num*num;
-Console.WriteLine(grid);
+﻿Console.WriteLine("Enter first number: ");
+int numfirst= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int numsecond= Convert.ToInt32(Console.ReadLine());
+int grid = numfirst*numfirst;
+//Console.WriteLine(numfirst);
+//Console.WriteLine(numsecond);
+//Console.WriteLine(grid);
+
+
+
+if (grid>numsecond)
+{
+   Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+
