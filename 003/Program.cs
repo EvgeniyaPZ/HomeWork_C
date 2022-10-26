9b0bfc4a011a58529428b64458df12d62944a671
Jalini <jalini@mail.ru>
2022-10-26 22:30:51 +0300

    jjsssssdsdsjjjkkhhhh

---
+++ 003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1=num%2;
+if (num1 ==0)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
