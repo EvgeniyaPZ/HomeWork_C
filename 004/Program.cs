c47b2d08f763ab0e3c9ceadff069b1e30770d1b3
Jalini <jalini@mail.ru>
2022-10-27 23:30:48 +0300

    jjsssssdsdsjjjkkhhhhl

---
+++ 004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <2)
+{
+    Console.WriteLine("error! Number must be more 1");
+}
+
+int count = 2;
+//int num1=num % 2;
+// Console.Write(num1);
+while (count <=num)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+        count=count+2;
+
+    }
