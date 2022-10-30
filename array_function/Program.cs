cbcb5bdef17cd6652e1e17be536482ad1e4157b4
Jalini <jalini@mail.ru>
2022-10-30 22:37:05 +0300

    array

---
+++ array_function/Program.cs
@@ -0,0 +1,28 @@
+﻿void FullArray (int[] collection)
+{
+    int lenth = collection.Length;
+    int index =0;
+
+    while (index<lenth)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[13];
+int[] array2 = new int[5];
+
+FullArray(array);
+PrintArray(array2);
