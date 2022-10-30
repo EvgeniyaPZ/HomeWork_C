799cfa325bf51639e4bc067a2cfcc493ee28e2e3
Jalini <jalini@mail.ru>
2022-10-30 22:36:20 +0300

    function

---
+++ task001_function/Program.cs
@@ -0,0 +1,26 @@
+﻿void FullArray1 (int[] array)
+{
+    int count = array.Length;
+    int index= 0;
+    while (index<count);
+    {
+        array[index]= new Random().Next(10,100);
+        index++;
+    }
+}
+
+void PrintArray1 (int[] arr)
+{
+    int count =arr.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(arr[position]);
+        position++;
+    }
+}
+
+int MaxFigure(int max)
+{
+    
+}
