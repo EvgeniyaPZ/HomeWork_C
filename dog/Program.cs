318b24ee2b2b5710e42bb68b56c3e9c4ae6ca421
Jalini <jalini@mail.ru>
2022-10-30 12:03:28 +0300

    task about dog and friends

---
+++ dog/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Dog's task!");
+Console.WriteLine("Speed first friend: ");
+int speedF1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Speed second friend: ");
+int speedF2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dog's speed: ");
+int speedDog = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Max distance: ");
+int maxD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Min distance: ");
+int minD = Convert.ToInt32(Console.ReadLine());
+int count =0;
+//int ffriend = 1;
+//int sfriend = 2;
+int who =1;
+int t = 0;
+
+
+while (maxD>minD)
+{
+    if (who==1)
+    {
+        t=maxD/(speedF2+speedDog);
+        count++;
+        maxD=t*(speedF1+speedF2);
+        who=2;
+    }
+    if (who==2)
+    {
+      t=maxD/(speedF1+speedDog);
+        count++;
+        maxD=t*(speedF1+speedF2);
+        who=2;  
+    }
+
+}
+Console.WriteLine(count);
