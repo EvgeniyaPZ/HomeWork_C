ee8192d2dd4be294bc81ac640b0b0e0adf10d653
Jalini <jalini@mail.ru>
2022-11-01 22:15:01 +0300

    dnumber1

---
+++ task011_dnumber/Program.cs
@@ -0,0 +1,16 @@
+﻿int CommonArray (int minValue, int maxValue)
+{
+        Random random=new Random();
+        int array=random.Next(minValue,maxValue);
+    return array;
+}
+int arr = CommonArray(100,1000);
+
+//Console.WriteLine(arr[5]);
+string arr1= arr.ToString();
+//string arr2= substing(1,3);
+//int arr2= arr%100;
+Console.Write(arr1[0]);
+Console.WriteLine(arr1[2]);
+Console.WriteLine(arr);
+
