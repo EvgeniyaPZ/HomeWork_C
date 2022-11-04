2b63ba146fb73d84b3f720fbf1f45d4bb138c823
Jalini <jalini@mail.ru>
2022-11-04 19:30:12 +0300

    723

---
+++ task013_723/Program.cs
@@ -0,0 +1,20 @@
+﻿int seven(int sev)
+{
+    int s = sev%7+sev%23;
+    //int t = sev%23;
+    //int r=
+    return s;
+}
+
+
+//int number =Convert.ToInt32(Console.WriteRead());
+int number=Convert.ToInt32(Console.ReadLine());
+//int lastNumber= seven(number)+eleven(number);
+if (seven(number)==0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
