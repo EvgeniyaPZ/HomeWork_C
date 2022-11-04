37caf57facf0657f2b9f5213627f3eee429da70f
Jalini <jalini@mail.ru>
2022-11-04 15:20:26 +0300

    multiple

---
+++ task012_multiple/Program.cs
@@ -0,0 +1,25 @@
+﻿int Multiple (int firstNumber, int secondNumber)
+{
+    
+        int result =firstNumber%secondNumber;
+ 
+    return result;
+}
+int fn=Convert.ToInt32(Console.ReadLine());
+int sn=Convert.ToInt32(Console.ReadLine());
+int res=Multiple(fn,sn);
+if (res==0)
+{
+    string res1= "кратно";
+    Console.WriteLine(res1);
+}
+else
+{
+    string res1= "некратно";
+    Console.Write(res1);
+    Console.Write(res);
+}
+
+
+
+
