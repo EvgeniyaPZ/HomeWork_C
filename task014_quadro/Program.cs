98046b4103218990da54dc9e63dbc5ad4ee591b8
Jalini <jalini@mail.ru>
2022-11-05 12:14:09 +0300

    quadro

---
+++ task014_quadro/Program.cs
@@ -0,0 +1,22 @@
+﻿int pro(int fn)
+{
+    int fn2=fn*fn;
+    return fn2;
+}
+Console.WriteLine("First number: ");
+int feN= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(pro(feN));
+
+Console.WriteLine("Second number: ");
+int seN= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(seN);
+//int num=pro(feN);
+
+if (pro(feN)==seN)
+{
+   Console.WriteLine("Yes");
+}
+else
+{
+   Console.WriteLine("No");
+}
